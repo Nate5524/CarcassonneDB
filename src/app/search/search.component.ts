bc43583318e44ce9091a3e34788d1de004a55367
Nate5524 <natebaker757@gmail.com>
2024-01-05 13:16:41 -0500

    refactored large repeating blocks

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -46,70 +46,10 @@ export class SearchComponent {
     this.tileList = this.tileFinderService.findTilesBySides(this.sidesSearch.toUpperCase());
   }
 
-  searchForMonastery(){
+  searchBooleanTrait(attribute: keyof Tile){
     let tempList : Tile[] = [];
     for(let tile of this.tileList){
-      if(tile.monastery){
-        tempList.push(tile);
-      }
-    }
-    this.tileList = tempList;
-  }
-
-  searchForShield(){
-    let tempList : Tile[] = [];
-    for(let tile of this.tileList){
-      if(tile.shield){
-        tempList.push(tile);
-      }
-    }
-    this.tileList = tempList;
-  }
-
-  searchForInn(){
-    let tempList : Tile[] = [];
-    for(let tile of this.tileList){
-      if(tile.inn){
-        tempList.push(tile);
-      }
-    }
-    this.tileList = tempList;
-  }
-
-  searchForCathedral(){
-    let tempList : Tile[] = [];
-    for(let tile of this.tileList){
-      if(tile.cathedral){
-        tempList.push(tile);
-      }
-    }
-    this.tileList = tempList;
-  }
-
-  searchForWheat(){
-    let tempList : Tile[] = [];
-    for(let tile of this.tileList){
-      if(tile.wheat){
-        tempList.push(tile);
-      }
-    }
-    this.tileList = tempList;
-  }
-
-  searchForCloth(){
-    let tempList : Tile[] = [];
-    for(let tile of this.tileList){
-      if(tile.cloth){
-        tempList.push(tile);
-      }
-    }
-    this.tileList = tempList;
-  }
-
-  searchForWine(){
-    let tempList : Tile[] = [];
-    for(let tile of this.tileList){
-      if(tile.wine){
+      if(tile[attribute]){
         tempList.push(tile);
       }
     }
@@ -117,57 +57,28 @@ export class SearchComponent {
   }
 
   applySearch(){
+    this.searchMessage = "";
     this.searchBySides();
-    if(this.monasterySearch){
-      this.searchForMonastery();
-    }
-    if(this.shieldSearch){
-      this.searchForShield();
-    }
-    if(this.innSearch){
-      this.searchForInn();
-    }
-    if(this.cathedralSearch){
-      this.searchForCathedral();
-    }
-    if(this.wheatSearch){
-      this.searchForWheat();
-    }
-    if(this.clothSearch){
-      this.searchForCloth();
-    }
-    if(this.wineSearch){
-      this.searchForWine();
-    }
-    this.generateSearchMessage();
-  }
-
-  generateSearchMessage(){
-    this.searchMessage = this.tileList.length + " tiles found";
     if(this.sidesSearch.length > 0){
-    this.searchMessage += " with sides \"" + this.sidesSearch + "\"";
+      this.searchMessage += " with sides \"" + this.sidesSearch + "\"";
     }
-    if(this.monasterySearch){
-      this.searchMessage += " with monasteries";
+    const boolSearchTraits: any[] = [
+      {attribute: "monastery", search: this.monasterySearch},
+      {attribute: "shield", search: this.shieldSearch},
+      {attribute: "inn", search: this.innSearch},
+      {attribute: "cathedral", search: this.cathedralSearch},
+      {attribute: "wheat", search: this.wheatSearch},
+      {attribute: "cloth", search: this.clothSearch},
+      {attribute: "wine", search: this.wineSearch}
+    ];
+
+    for (let trait of boolSearchTraits){
+      if (trait.search){
+        this.searchBooleanTrait(trait.attribute);
+      }
     }
-    if(this.shieldSearch){
-      this.searchMessage += " with shields";
-    }
-    if(this.innSearch){
-      this.searchMessage += " with inns";
-    }
-    if(this.cathedralSearch){
-      this.searchMessage += " with cathedrals";
-    }
-    if(this.wheatSearch){
-      this.searchMessage += " with wheat";
-    }
-    if (this.clothSearch){
-      this.searchMessage += " with cloth";
-    }
-    if (this.wineSearch){
-      this.searchMessage += " with wine";
-    }
+    this.searchMessage = this.tileList.length + " tiles found" + this.searchMessage;
+    return;
   }
 
   clearSearch(){
